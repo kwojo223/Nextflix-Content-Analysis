7b2f0047a82f2f700eed80fa0fa8b4065bd17dbf
kwojo <ko295@kent.ac.uk>
2026-07-09 22:01:41 +0100

    Add SQL analysis queries

--- sql/netflix_analysis.sql
+++ sql/netflix_analysis.sql
@@ -1,4 +1,42 @@
--- queries.sql
-SELECT title, type
-FROM netflix_titles
-LIMIT 5;
+-- Count Movies vs TV Shows
+SELECT
+    type,
+    COUNT(*) AS total_titles
+FROM netflix
+GROUP BY type;
+
+-- Top 10 countries producing Netflix content
+
+SELECT
+    country,
+    COUNT(*) AS total_titles
+FROM netflix
+WHERE country IS NOT NULL
+GROUP BY country
+ORDER BY total_titles DESC
+LIMIT 10;
+-- Number of titles released each year
+
+SELECT
+    release_year,
+    COUNT(*) AS total_titles
+FROM netflix
+GROUP BY release_year
+ORDER BY release_year;
+-- Distribution of ratings
+
+SELECT
+    rating,
+    COUNT(*) AS total_titles
+FROM netflix
+GROUP BY rating
+ORDER BY total_titles DESC;
+-- Genres on Netflix
+
+SELECT
+    listed_in,
+    COUNT(*) AS total_titles
+FROM netflix
+GROUP BY listed_in
+ORDER BY total_titles DESC
+LIMIT 10;
